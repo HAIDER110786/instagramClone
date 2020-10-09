bd645ec66e0b70e10d18da3fa6fb9c6b7e0802fa
syed haider imam abidi <abidihaider89@gmail.com>
2020-10-10 03:56:47 +0500

    removedCommentsFromAppJS

--- App.js
+++ App.js
@@ -1,9 +1,4 @@
 import React from 'react';
-// import Header from './components/header';
-// import Taskbar from './components/taskbar';
-// import Statusbar from './components/statusbar';
-// import Posts from './components/posts';
-// import Footer from './components/footer';
 import Temp from './components/fileBeforeAppJS';
 import { View, StyleSheet } from 'react-native';
 
